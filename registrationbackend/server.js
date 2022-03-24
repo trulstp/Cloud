191779b31a18e5e9943b0aa982ed406f55ebf979
trulstp <trulstp@stud.ntnu.no>
2022-03-24 14:34:06 +0100

    tryingagainnn12342

--- registrationbackend/server.js
+++ registrationbackend/server.js
@@ -4,12 +4,13 @@ const mongoose = require('mongoose')
 const dotenv = require('dotenv')
 const routesUrls = require('./routes/routes')
 const cors = require('cors')
+const path = require('path')
 
 
 dotenv.config()
 
 mongoose.connect(process.env.DATABASE_ACCESS, () =>console.log("Database connected"))
-
+app.use(express.static(path.resolve(__dirname, "./client/build")));
 app.use(express.json())
 app.use(cors())
 app.use('/app', routesUrls)
